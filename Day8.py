d42af8e33c7f0788a606c48588403ffe5bb532de
0x000C <0x000C@walker.lt>
2022-12-10 18:59:22 -0600

    Cleanup Day8

--- Day8.py
+++ Day8.py
@@ -1,25 +1,20 @@
 class Tree:
-    def __init__(self, size = 0, seen = False, score = 1):
+    def __init__(self, size=0, seen=False, score=1):
         self.size = size
         self.seen = seen
         self.score = score
 
-with open("Inputs/Day8.txt") as datafile:
-    lines = [i.removesuffix('\n') for i in datafile.readlines()]
-    inputdata = []
-    for i in lines:
-        inputdata.append([Tree(int(x)) for x in i])
 
 def part1(treegrid):
     for edge in range(4):
-        for rownum in range(len(treegrid)):
-            tallesttreesize = treegrid[rownum][0].size
-            treegrid[rownum][0].seen = True
-            for treenum in range(len(treegrid[rownum])):
-                if treegrid[rownum][treenum].size > tallesttreesize:
-                    treegrid[rownum][treenum].seen = True
-                    tallesttreesize = treegrid[rownum][treenum].size
-        treegrid = list(zip(*treegrid[::-1]))
+        for row in treegrid:
+            tallesttreesize = row[0].size
+            row[0].seen = True
+            for treenum in range(len(row)):
+                if row[treenum].size > tallesttreesize:
+                    row[treenum].seen = True
+                    tallesttreesize = row[treenum].size
+        treegrid = list(zip(*treegrid[::-1])) #rotate the grid
 
     edgevisibletrees = 0
     for i in treegrid:
@@ -28,19 +23,18 @@ def part1(treegrid):
                 edgevisibletrees += 1
     return edgevisibletrees
 
+
 def part2(treegrid):
     for edge in range(4):
         for row in treegrid:
             for treenum in range(len(row)):
                 score = 0
-                for othertreenum in range(treenum-1, -1, -1):
-                    if row[treenum].size > row[othertreenum].size:
-                        score += 1
-                    else:
-                        score += 1
+                for othertreenum in range(treenum - 1, -1, -1):
+                    score += 1
+                    if row[treenum].size <= row[othertreenum].size:
                         break
                 row[treenum].score *= score
-        treegrid = list(zip(*treegrid[::-1]))
+        treegrid = list(zip(*treegrid[::-1])) 
 
     maxscore = 0
     for x in treegrid:
@@ -49,5 +43,14 @@ def part2(treegrid):
                 maxscore = y.score
     return maxscore
 
+def get_data(inputfile):
+    with open(inputfile) as datafile:
+        lines = [i.removesuffix('\n') for i in datafile.readlines()]
+        data = []
+        for i in lines:
+            data.append([Tree(int(x)) for x in i])
+    return data
+
+inputdata = get_data("Inputs/Day8.txt")
 print("Part 1:", part1(inputdata))
 print("Part 2:", part2(inputdata))
